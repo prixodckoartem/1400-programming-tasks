c3848d505ee2d5093377cd3efbd382deb09ee6a7
Artem <prixodckoartem@gmail.com>
2025-09-23 12:00:00 +0300

    Глава 2

---
+++ Вычисления по формулам Глава 2.js
@@ -0,0 +1,59 @@
+//2.1 a)
+let x = parseFloat(prompt("Введите значение x:"));
+
+let y = 17 * Math.pow(x, 2) - 6 * x + 13;
+
+alert("При x = " + x + ", значение функции y = " + y);
+
+
+console.log("При x = " + x + ", значение функции y = " + y);
+
+//2.1 b)
+let a = parseFloat(prompt("Введите значение a:"));
+
+let y = 3 * Math.pow(a, 2) + 5 * a - 21;
+
+alert("При a = " + a + ", значение функции y = " + y);
+
+console.log("При a = " + a + ", значение функции y = " + y);
+
+
+
+ function calculateFunction(a) {
+     return Math.pow(a, 2) + 10;
+ }
+
+// 2.2
+ function functionA(a) {
+     return 2 * Math.pow(a, 2) + 3 * a - 1;
+ }
+
+// 2.3
+ function functionB(x) {
+     return Math.sin(x) + Math.cos(x);
+ }
+
+// 2.4
+ function squarePerimeter(side) {
+     return 4 * side;
+ }
+
+//2.5
+ function circleDiameter(radius) {
+     return 2 * radius;
+ }
+
+//2.6
+ function horizonDistance(height) {
+     const R = 6350;
+     return Math.sqrt(Math.pow(R + height, 2) - Math.pow(R, 2));
+ }
+
+ console.log(horizonDistance(0.1));
+
+//2.7
+ function cubeProperties(edge) {
+     const volume = Math.pow(edge, 3);
+     const surfaceArea = 6 * Math.pow(edge, 2);
+     return { volume, surfaceArea };
+ }
